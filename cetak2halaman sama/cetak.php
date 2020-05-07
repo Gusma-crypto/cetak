bd59fdcdf261ad61e746fb620aaa04aee150fc3d
Gusma-crypto <agussulistionoa0@gmail.com>
2020-05-08 00:15:02 +0700

    menambahkan cetak 2 halaman A4

---
+++ cetak2halaman sama/cetak.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cetak</title>
+</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+	<div class="book">
+	    <div class="page">
+	        <div class="subpage">
+	        	<center>
+					<h1>Cetak 2 halaman tidak terputus</h1>
+				</center>
+	        Page 1/2
+	        <p></p>
+	    </div>    
+	    </div>
+	    <div class="page">
+	        <div class="subpage">Page 2/2</div>    
+	    </div>
+	</div>
+	<script>
+		window.print();
+	</script>
+</body>
+</html>
---
+++ cetak2halaman sama/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>cetak eda halaman</title>
+</head>
+<body>
+	<a href="cetak.php" target="_blank">CETAK REAM MEDIS</a>
+</body>
+</html>
